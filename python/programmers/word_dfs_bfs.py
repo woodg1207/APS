3ad51410a224785c8d475229a341be4f0bc008a8
woodg1207 <woodg1207@gmail.com>
2020-09-15 17:45:44 +0900

    aps | solve word

--- python/programmers/word_dfs_bfs.py
+++ python/programmers/word_dfs_bfs.py
@@ -1,11 +1,29 @@
 def solution(begin, target, words):
     n = len(begin)
-    visit = [False]*len(words)
-    def dfs(w):
-        return
-    
-
-    return begin
+    nw = len(words)
+    combi = [[j for j in range(n) if i!=j] for i in range(n)]
+    visit = [False]*nw
+    l=[]
+    def dfs(w, cnt):
+        if w==target:
+            l.append(cnt)
+            return
+        for i in range(nw):
+            if visit[i]:continue
+            for j in range(len(combi)):
+                flag = 0
+                for k in combi[j]:
+                    if w[k]!=words[i][k]:
+                        flag=1
+                        break
+                if flag: continue
+                visit[i]=True
+                dfs(words[i], cnt+1)
+                visit[i]=False
+        
+    dfs(begin,0)
+    if l: return min(l)
+    return 0
 
 B='hit'
 T='cog'
