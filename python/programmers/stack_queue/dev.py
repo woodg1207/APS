d79263b01dd5fe281ebd40b1013b48a4cb37e8bd
woodg1207 <woodg1207@gmail.com>
2020-09-17 21:20:18 +0900

    add aps

--- python/programmers/stack_queue/dev.py
+++ python/programmers/stack_queue/dev.py
@@ -1,5 +1,32 @@
+from collections import deque
+
 def solution(progresses, speeds):
     answer = []
+    n = len(progresses)
+    done = [False] * n 
+    q = deque()
+    speed = deque()
+    release = deque()
+    q.extend(progresses)
+    speed.extend(speeds)
+    release.extend(done)
+    while q:
+        for i in range(len(q)):
+            q[i] += speed[i]
+            if q[i]>=100:
+                release[i] = True
+        if release[0]:
+            cnt = 0
+            for i in range(len(release)):
+                if release[i]:
+                    q.popleft()
+                    speed.popleft()
+                    cnt += 1
+                else:
+                    break
+            for i in range(cnt):
+                release.popleft()
+            answer.append(cnt)
     return answer
 
 
