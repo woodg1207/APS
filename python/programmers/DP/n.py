5363b3d06f144659c983591d89c63246e5f32a23
woodg1207 <woodg1207@gmail.com>
2020-10-08 17:20:04 +0900

    add python

---
+++ python/programmers/DP/n.py
@@ -0,0 +1,20 @@
+def solution(N, number):
+    memory = [[False for i in range(8)]]*1000000
+    def dfs(result, cnt):
+        if cnt>8:
+            return
+        for i in range(5):
+            if not i:
+                result = int(str(result)+str(N))
+                dfs(result, cnt+1)
+            elif i==1:
+                result += N
+                dfs(result, cnt)
+            
+
+        return
+    return 0
+
+n = 5
+Num = 12
+print(solution(n, Num))
