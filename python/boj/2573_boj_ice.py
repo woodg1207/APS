0f2cb0bd1367f177c42599dd329f8dd567ea820c
woodg1207 <woodg1207@gmail.com>
2020-10-23 14:58:09 +0900

    add aps ice

--- python/boj/2573_boj_ice.py
+++ python/boj/2573_boj_ice.py
@@ -1,55 +1,73 @@
 import sys; sys.stdin = open('s2573.txt','r')
 
-import sys; sys.setrecursionlimit(10**6)
-from copy import deepcopy
+import sys; sys.setrecursionlimit(10**9)
+
+from collections import deque
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 Y = 0
-dr = [1, 0, -1, 0]
-dc = [0, 1, 0, -1]
+flag = 0
+memo = []
+dr = (1, 0, -1, 0)
+dc = (0, 1, 0, -1)
 
 def dfs(r, c):
-    visit[r][c]=True
+    visit[r][c] = True
+    water = 0
     for i in range(4):
-        nr, nc = r+dr[i], c+dc[i]
+        nr, nc = r + dr[i], c + dc[i]
         if 0<=nr<N and 0<=nc<M:
             if visit[nr][nc]: continue
-            elif arr[nr][nc]<=0:
-                dfs(nr,nc)
-            elif arr[nr][nc]:
-                visit[nr][nc]=True
-                arr[nr][nc]-=1
-
-def dfs2(r, c):
-    visit1[r][c]=True
-    for i in range(4):
-        nr, nc = r+dr[i], c+dc[i]
-        if 0<=nr<N and 0<=nc<M:
-            if visit1[nr][nc]: continue
-            elif arr[nr][nc]>0:
-                dfs2(nr,nc)
+            if arr[nr][nc]:
+                dfs(nr, nc)
+            else:
+                water += 1
+    memo.append((r, c, water))
 
 while 1:
-    Y+=1
-    visit = [[False]*M for _ in range(N)]
-    visit1 = deepcopy(visit)
-    dfs(0,0)
-    cnt = 0
-    flag = 0
+    visit = [[False] * M for _ in range(N)]
+    cnt_dfs = 0
     for i in range(N):
         for j in range(M):
+            if visit[i][j]: continue
             if arr[i][j]:
-                if visit1[i][j]: continue
-                dfs2(i,j)
-                cnt += 1
-            if cnt >1:
-                flag = 1
-                break
+                cnt_dfs += 1
+                if cnt_dfs>1:
+                    flag = 1
+                    break
+                # dfs(i, j)
+                q = deque()
+                q.append((i, j))
+                visit[i][j] = True
+                while q:
+                    r, c = q.popleft()
+                    # visit[r][c] = True
+                    water = 0
+                    for d in range(4):
+                        nr, nc = r + dr[d], c + dc[d]
+                        if 0<=nr<N and 0<=nc<M:
+                            if visit[nr][nc]: continue
+                            if arr[nr][nc]:
+                                q.append((nr, nc))
+                                visit[nr][nc] = True
+                            else:
+                                water += 1
+                    memo.append((r, c, water))
         if flag:
             break
+    else:
+        if not cnt_dfs:
+            flag = 2
+    if not flag:
+        for info in memo:
+            arr[info[0]][info[1]] -= info[2]
+            if arr[info[0]][info[1]] < 0:
+                arr[info[0]][info[1]] = 0
     if flag:
+        if flag == 2:
+            print(0)
+            break
         print(Y)
         break
-    elif cnt == 0:
-        print(0)
-        break
+    Y += 1
+    memo.clear()
