d292da4b25b0c908dbb0cad23851f1151ba061fd
woodg1207 <woodg1207@gmail.com>
2020-10-27 19:48:45 +0900

    a

--- python/boj/10815_boj_Ncard.py
+++ python/boj/10815_boj_Ncard.py
@@ -2,15 +2,14 @@ import sys; sys.stdin = open('s10815.txt', 'r')
 
 N = int(input())
 sang = dict()
-for i in list(input().split()):
+for i in input().split():
     sang[i] = 1
 M = int(input())
 result = []
 
-for i in list(input().split()):
+for i in input().split():
     try:
         result.append(sang[i])
     except:
         result.append(0)
-    finally:
-        print(result[-1], end=' ')
+print(*result)
