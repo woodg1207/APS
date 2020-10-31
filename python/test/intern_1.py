a785f76c623786e16197d8926ce9f54651747e16
woodg1207 <woodg1207@gmail.com>
2020-10-31 15:55:10 +0900

    add

---
+++ python/test/intern_1.py
@@ -0,0 +1,31 @@
+def solution(n, delivery):
+    visit = [0] * (n+1)
+    for i in range(len(delivery)):
+        if delivery[i][2]:
+            visit[delivery[i][0]] = 1
+            visit[delivery[i][1]] = 1
+    for i in range(len(delivery)):
+        if not delivery[i][2]:
+            flag = 0
+            if visit[delivery[i][0]]==1:
+                visit[delivery[i][1]] = 2
+            elif visit[delivery[i][1]]==1:
+                visit[delivery[i][0]] = 2                                    
+            
+                
+    result = ''
+    print(visit)
+    for i in range(1, n+1):
+        if visit[i] == 0:
+            result += '?'
+        elif visit[i] == 1:
+            result += 'O'
+        elif visit[i] == 2:
+            result += 'X'
+
+
+    return result
+
+N = 6
+D = [[1,3,1],[3,5,0],[5,4,0],[2,5,0]]
+print(solution(N, D))
---
+++ python/test/intern_2.py
@@ -0,0 +1,38 @@
+def solution(encrypted_text, key, rotation):
+    x = 'abcdefghijklmnopqrstuvwxyz'
+    e = []
+    k = []
+    abc = dict()
+    num = dict()
+    for i in range(26):
+        abc[x[i]] = i+1
+        num[i+1] = x[i]
+    flag = 0
+    if rotation > 0 :
+        flag = 1  # 왼쪽으로 이동
+    s = ''
+    if flag: 
+        s = encrypted_text[rotation:] + encrypted_text[:rotation]
+    else:
+        rotation += len(key)
+        s =  encrypted_text[:rotation] +encrypted_text[rotation:]
+    
+    encrypted_text = s
+    for i in range(len(key)):
+        e.append(abc[encrypted_text[i]])
+        k.append(abc[key[i]])
+    for i in range(len(key)):
+        e[i] -= k[i]
+    result = ''
+    for i in range(len(e)):
+        x = e[i]%26
+        if not x:
+            x = 26
+        result += num[x]
+    return result
+
+
+E = 'qyyigoptvfb'
+K = 'abcdefghijk'
+R = 3
+print(solution(E, K, R))
