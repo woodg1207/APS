bcad59f46580ea7a35f36280a1d1fb92a8fbb25e
woodg1207 <woodg1207@gmail.com>
2020-11-20 00:54:01 +0900

    add aps programmers

--- python/programmers/summer_winter_2019/rectangle.py
+++ python/programmers/summer_winter_2019/rectangle.py
@@ -3,7 +3,19 @@ def solution(w,h):
         w, h = h, w
     elif h==w:
         return h*w-h
-    return h*w - (((w//h)+1)*h)
-
-
-print(solution(5,2))
+    def new_round(num):
+        if num == 0:
+            return 0 
+        if num % 1:
+            return num//1
+        else:
+            return num - 1
+    diag = w/h
+    cnt = 0
+    for i in range(h):
+        cnt += new_round(diag*(i+1))-new_round(diag*i) + 1
+        print(new_round(diag*(i+1)),new_round(diag*i), cnt)
+    return h*w - cnt
+print(solution(8,12))
+# print(solution(5,2))
+# print(solution(500,2))
