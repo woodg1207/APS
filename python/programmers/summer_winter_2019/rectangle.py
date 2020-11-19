9a80552aad924ced199942b44d8a40f08f0ae507
woodg1207 <woodg1207@gmail.com>
2020-11-19 22:21:25 +0900

    add aps

--- python/programmers/summer_winter_2019/rectangle.py
+++ python/programmers/summer_winter_2019/rectangle.py
@@ -1,8 +1,9 @@
 def solution(w,h):
-    answer = 1
-    ## 삼각함수를 사용해서 알고리즘을 측정한다.?
-    seta = h/w
-    return answer
+    if h>w:
+        w, h = h, w
+    elif h==w:
+        return h*w-h
+    return h*w - (((w//h)+1)*h)
 
 
-print(solution(8,12))
+print(solution(5,2))
