964b4c44fc4fdf8b5ff74054f0beef3f9ba3bd72
woodg1207 <woodg1207@gmail.com>
2020-12-10 22:53:18 +0900

    add crane

--- java/programmers/Crane.java
+++ java/programmers/Crane.java
@@ -1,23 +1,62 @@
+import java.util.ArrayList;
 class Solution1 {
     public static int solution(int[][] board, int[] moves) {
+        ArrayList<Integer> basket = new ArrayList<>();
         int answer = 0;
-        int[] memory = new int[board[0].length];
-        int doll;
-        for (int i=0; i<memory.length; i++){
-            memory[i] = 32;
+        int[] memory = new int[board.length];
+        int doll=0;
+        for (int i=0; i<board.length;i++){
+            for (int j=0; j<board.length;j++){
+                System.out.printf("%d ", board[i][j]);
+            }
+            System.out.println();
         }
-
+        
+        for (int i=0; i<memory.length; i++){
+            memory[i] = 31;
+        }
         for(int move:moves){
-            if (memory[move]==32){
+            move--;
+            System.out.println("move : "+move);
+            doll = 0;
+            if (memory[move]==31){
                 for (int i=0; i<board.length;i++){
                     if (board[i][move]!=0){
                         doll = board[i][move];
+                        board[i][move] = 0;
                         memory[move]=i+1;
+                        break;
+                    }
+                }
+            } else if(memory[move] <= board.length-1){
+                doll = board[memory[move]][move];
+                board[memory[move]][move] = 0;
+                memory[move]++;
+            }
+            if (doll!=0) {
+                if (basket.size()==0){
+                    basket.add(doll);
+                } else {
+                    int predoll = basket.get(basket.size()-1);
+                    basket.remove(basket.size()-1);
+                    if (predoll!=doll){
+                        basket.add(predoll);
+                        basket.add(doll);
+                    } else {
+                        answer+=2;
                     }
                 }
             }
+            
+            System.out.println("doll : "+doll+" answer : "+answer);
+            System.out.println(basket);
+            for (int i=0; i<board.length;i++){
+                for (int j=0; j<board.length;j++){
+                    System.out.printf("%d ", board[i][j]);
+                }
+                System.out.println();
+            }
         }
-
         return answer;
     }
 }
