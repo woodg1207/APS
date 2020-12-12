e2a36392243bcb7c690043c88212174bdde19bd4
woodg1207 <woodg1207@gmail.com>
2020-12-12 14:01:24 +0900

    boj bfs aps

---
+++ python/boj/19952_boj_insung.py
@@ -0,0 +1,12 @@
+import sys; sys.stdin = open('s19952.txt', 'r')
+from pprint import pprint
+
+tcs = int(input())
+dr = [1, 0, -1, 0]
+dc = [0, 1, 0, -1]
+for tc in range(tcs):
+    H, W, O, F, Rs, Cs, Re, Ce = map(int, input().split())
+    arr = [[0]*W for _ in range(H)]
+    for i in range(O):
+        x, y, l = map(int, input().split())
+        arr[x-1][y-1] = l
