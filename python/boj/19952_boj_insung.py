701bd8f64083d673dc3a5951fe719b2a4dce0022
woodg1207 <woodg1207@gmail.com>
2020-12-15 15:22:24 +0900

    boj | 19952 python

--- python/boj/19952_boj_insung.py
+++ python/boj/19952_boj_insung.py
@@ -1,22 +1,7 @@
 import sys; sys.stdin = open('s19952.txt', 'r')
 from pprint import pprint
 
-def dfs(r, c, f):
-    w = 0
-    # print(r, c, f)
-    if r==Re and c == Ce: return 1
-    if f<=0: return 0
-    for i in range(4):
-        nr, nc = r + dr[i], c + dc[i]
-        if 0<=nr<H and 0<=nc<W:
-            if visit[nr][nc]: continue
-            if arr[r][c]<arr[nr][nc]: # jump
-                if f < arr[nr][nc]-arr[r][c]: continue
-            visit[nr][nc] = 1
-            w+=dfs(nr, nc, f-1)
-            visit[nr][nc] = 0
-            if w: return w
-    return w
+from collections import deque
 tcs = int(input())
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
@@ -29,10 +14,46 @@ for tc in range(tcs):
     for i in range(O):
         x, y, l = map(int, input().split())
         arr[x-1][y-1] = l
-    # print('testcase : '+str(tc+1))
-    # print(H, W, O, F, Rs, Cs, Re, Ce)
-    # pprint(arr)
-    if dfs(Rs, Cs, F):
+    q = deque()
+    q.append((Rs, Cs, F))
+    flag = 0
+    while q:
+        r, c, f = q.popleft()
+        if r==Re and c==Ce:
+            flag = 1
+            break
+        if not f:
+            continue
+        # visit[r][c] = 1
+        for i in range(4):
+            nr, nc = r+dr[i], c+dc[i]
+            if 0<=nr<H and 0<=nc<W:
+                if visit[nr][nc]: continue
+                if f<arr[nr][nc]-arr[r][c]: continue
+                visit[nr][nc] = 1
+                q.append((nr, nc, f-1))
+    if flag:
         print('잘했어!!')
     else:
         print('인성 문제있어??')
+# dfs로 하면 시간 초과.
+# def dfs(r, c, f):
+#     w = 0
+#     # print(r, c, f)
+#     if r==Re and c == Ce: return 1
+#     if f<=0: return 0
+#     for i in range(4):
+#         nr, nc = r + dr[i], c + dc[i]
+#         if 0<=nr<H and 0<=nc<W:
+#             if visit[nr][nc]: continue
+#             if arr[r][c]<arr[nr][nc]: # jump
+#                 if f < arr[nr][nc]-arr[r][c]: continue
+#             visit[nr][nc] = 1
+#             w+=dfs(nr, nc, f-1)
+#             visit[nr][nc] = 0
+#             if w: return w
+#     return w
+    # if dfs(Rs, Cs, F):
+    #     print('잘했어!!')
+    # else:
+    #     print('인성 문제있어??')
