cee6e44058337c778e8bdab7983b91dba10f540f
woodg1207 <woodg1207@gmail.com>
2021-01-05 18:15:28 +0900

    skill tree aps

--- python/programmers/summer_winter_2018/skilltree.py
+++ python/programmers/summer_winter_2018/skilltree.py
@@ -1,5 +1,30 @@
+eng = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+eng_dict = dict()
+num_dict = dict()
+for i in range(26):
+    eng_dict[i] = eng[i]
+    num_dict[eng[i]] = i
 def solution(skill, skill_trees):
+    priority = [0]*26
     answer = 0
+    for i in range(len(skill)):
+        priority[num_dict[skill[i]]] = i+1
+    for skill_tree in skill_trees:
+        cnt = 0
+        flag = 0
+        for s in skill_tree:
+            if priority[num_dict[s]]:
+                if priority[num_dict[s]] - cnt == 1:
+                    cnt += 1
+                else:
+                    flag = 1
+            if flag:
+                break
+        if flag:
+            continue
+        else:
+            answer += 1
+            
     return answer
 
 print(solution("CBD",["BACDE", "CBADF", "AECB", "BDA"]))
