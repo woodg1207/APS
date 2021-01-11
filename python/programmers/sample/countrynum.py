587773f604071b03cabfe856927414cd195df62f
woodg1207 <woodg1207@gmail.com>
2021-01-12 01:38:36 +0900

    aps

--- python/programmers/sample/countrynum.py
+++ python/programmers/sample/countrynum.py
@@ -1,6 +1,14 @@
 def solution(n):
     answer = ''
-    return answer
+    l = 3
+    cnt = 1
+    while n>l:
+        cnt += 1
+        l += l*3
+    n -= 3**(cnt-1)
+    for i in range(cnt):
+        
+    return cnt 
 
 
-print(solution(4))
+print(solution(13))
