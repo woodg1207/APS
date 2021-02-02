2d9f87c795a51cd961c06760b821abc048ed9e0c
woodg1207 <woodg1207@gmail.com>
2021-02-02 19:17:14 +0900

    kakao aps lock&key

--- python/programmers/2020_kakao_blind/LockKey.py
+++ python/programmers/2020_kakao_blind/LockKey.py
@@ -10,19 +10,32 @@ def rotate(arr, c, cnt):
         result.append(temp)
     return rotate(result, c, cnt+1)
 
+def moveCheck(mr, mc, k, l):
+    N, M = len(l), len(k)
+    for i in range(N):
+        for j in range(N):
+            x = l[i][j]
+            if 0 <= i-mr < M and 0 <= j-mc < M:
+                x += k[i-mr][j-mc]
+            if x!=1: return False
+            
+    return True
+
 def solution(key, lock):
     N = len(lock)
-    M = len(key)
     for r in range(4):
         k = rotate(key, r, 0)
         ## 위치이동 시키면서 맞추기.
-        for i in range(N):
-            for j in range(N):
-                pass
-
-    return 0
+        for mr in range(-N+1, N):
+            for mc in range(-N+1,N):
+                if moveCheck(mr, mc, k, lock): return True
+    return False
 
 print(solution(
     [[0, 0, 0], [1, 0, 0], [0, 1, 1]], 
     [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
     ))
+# print(solution(
+#     [[1, 1, 1], [1, 1, 0], [1, 0, 1]],
+#     [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+#     ))
