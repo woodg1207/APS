62ba5c300f9eaa2835a1b90ec81d8fd2c438938a
woodg1207 <woodg1207@gmail.com>
2021-02-04 18:05:10 +0900

    add pillar

--- python/programmers/2020_kakao_blind/pillar.py
+++ python/programmers/2020_kakao_blind/pillar.py
@@ -1,6 +1,6 @@
+from pprint import pprint
 def create_p(x, y, w):
     if y == 0:
-        # w['{}{}0'.format(x,y)] = 1
         return 1
     else:
         if w.get('{}{}0'.format(x,y-1)) or w.get('{}{}1'.format(x-1,y)) or w.get('{}{}1'.format(x, y)):
@@ -12,22 +12,35 @@ def create_b(x, y, w):
         return 1
     return 0 
 
-def delete(x, y, z, l):
-    ## 기록들을 통해서 순서대로 만드는데 특정 기록은 스킵하고 
-    # 만들때 한곳이라도 만들수 없으면 못만드는것을 리턴해주도록 만들기
-    w = dict()
-    for i in range(len(l)):
-        if [x, y, z] == l[i]:
-            continue
-        if l[i][2]:
-            if not create_b(l[i][0], l[i][1], w):
-                return 1
-            w['{}{}1'.format(l[i][0], l[i][1])] = 1
-        else:
-            if not create_p(l[i][0], l[i][1], w):
-                return 1
-            w['{}{}0'.format(l[i][0], l[i][1])] = 1
-    return 0
+def delete(x, y, a, w):
+    if w.get('{}{}{}'.format(x, y, a)):
+        w['{}{}{}'.format(x, y, a)] = 0
+    else:
+        return 1
+    if a: #bo
+        if w.get('{}{}{}'.format(x-1, y, 1)):
+            if not create_b(x-1, y, w):
+                return 0
+        if w.get('{}{}{}'.format(x, y, 0)):
+            if not create_p(x, y, w):
+                return 0
+        if w.get('{}{}{}'.format(x+1, y, 1)):
+            if not create_b(x+1, y, w):
+                return 0
+        if w.get('{}{}{}'.format(x+1, y, 0)):
+            if not create_b(x+1, y, w):
+                return 0
+    else:
+        if w.get('{}{}{}'.format(x+1,y+1,1)):
+            if not create_b(x+1, y+1, w):
+                return 0
+        if w.get('{}{}{}'.format(x,y+1,0)):
+            if not create_p(x, y+1, w):
+                return 0
+        if w.get('{}{}{}'.format(x,y+1,1)):
+            if not create_b(x, y+1, w):
+                return 0
+    return 1
 
 
 def convert(w):
@@ -39,26 +52,23 @@ def convert(w):
 
 def solution(n, build_frame):
     wall = dict()
+    sample = []
     for i in range(len(build_frame)):
         x, y, a, b = build_frame[i]
-        sample = []
         if b: # create
             if a: # 1 = bo
                 if create_b(x, y, wall):
                     wall['{}{}1'.format(x, y)] = 1
-                    sample.append([x, y, 1])
             else: # call pillar create
                 if create_p(x, y, wall):
                     wall['{}{}0'.format(x, y)] = 1
-                    sample.append([x, y, 0])
-        else: # delete
-            if delete(x, y, a, sample):
+        else: # delete b = 0
+            if delete(x, y, a, wall):
                 wall['{}{}{}'.format(x, y, a)] = 0
-                temp = []
-                for idx in range(len(sample)):
-                    if [x, y, a] == sample[idx]: continue
-                    temp.append(sample[idx])
-                sample = temp
+            else:
+                wall['{}{}{}'.format(x, y, a)] = 1
+            # print(wall, (x, y, a), '##') 
     return convert(wall)
 
 print(solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]))
+print(solution(5, [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]))
