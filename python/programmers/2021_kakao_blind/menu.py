5d6b828e5bdfbad88921308ed81283d4706b81f4
woodg1207 <woodg1207@gmail.com>
2021-03-06 18:57:25 +0900

    2021 kakao

---
+++ python/programmers/2021_kakao_blind/menu.py
@@ -0,0 +1,11 @@
+def comb():
+    
+    return
+def solution(orders, course):
+    answer = []
+
+    return answer
+
+
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
