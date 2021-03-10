d995de78e3969e6285671d42418eed162f9b347a
woodg1207 <woodg1207@gmail.com>
2021-03-10 19:21:29 +0900

    menu

--- python/programmers/2021_kakao_blind/menu.py
+++ python/programmers/2021_kakao_blind/menu.py
@@ -1,9 +1,13 @@
-def comb():
-    
+def comb(o, c, cnt):
+    for i in range(cnt, len(o)):
+        pass        
     return
 def solution(orders, course):
     answer = []
-
+    for order in orders:
+        for i in course:
+            comb(order, i, 0)
+    
     return answer
 
 
