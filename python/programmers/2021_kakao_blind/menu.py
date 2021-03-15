87e3e13722609e32bbf5466a1ad429af42534ace
woodg1207 <woodg1207@gmail.com>
2021-03-15 22:02:02 +0900

    kakao

--- python/programmers/2021_kakao_blind/menu.py
+++ python/programmers/2021_kakao_blind/menu.py
@@ -1,14 +1,39 @@
-def comb(o, c, cnt):
-    for i in range(cnt, len(o)):
-        pass        
-    return
+abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numToAbc = dict()
+abcToNum = dict()
+for i in range(len(abc)):
+    numToAbc[i] = abc[i]
+    abcToNum[abc[i]] = i
+
+def comb(o, c, idx, cnt):
+    if c == cnt:
+        print(o[idx])
+        return [o[idx]]
+    r = [o[idx]]
+    for i in range(idx+1, len(o)):
+        r.extend(comb(o, c, i, cnt+1))
+        r.pop()
+    return r
+
+def convertNum(order):
+    r = []
+    for i in range(len(order)):
+        r.append(abcToNum[order[i]])
+    return sorted(r)
+
 def solution(orders, course):
-    answer = []
-    for order in orders:
-        for i in course:
-            comb(order, i, 0)
     
-    return answer
+    answer = []
+    for l in course:
+        sampleMenu = []
+        for order in orders:
+            # order = convertNum(order)
+            if l > len(order): continue
+            sampleMenu.append(comb(order, 3, 0, 1))
+            break
+        break
+    
+    return sampleMenu
 
 
 
