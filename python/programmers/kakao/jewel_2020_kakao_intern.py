5e2e7decf5438b608f9f4ece2d67388d27c3a40c
woo <woodg1207@gmail.com>
2021-06-20 18:06:21 +0900

    add jewel python

---
+++ python/programmers/kakao/jewel_2020_kakao_intern.py
@@ -0,0 +1,15 @@
+
+
+def solution(gems):
+    visit = list(set(gems))
+    visit_bool = [False]*(len(visit))
+    for i in range(len(gems)-len(visit)+1):
+        
+
+
+
+    return 
+
+
+G = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+print(solution(G))
