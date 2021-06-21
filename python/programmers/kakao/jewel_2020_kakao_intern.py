e49a6fb01e1a51213963cd58f13a1364833482f8
woo <woodg1207@gmail.com>
2021-06-21 13:17:19 +0900

    jewel

--- python/programmers/kakao/jewel_2020_kakao_intern.py
+++ python/programmers/kakao/jewel_2020_kakao_intern.py
@@ -1,14 +1,25 @@
-
-
 def solution(gems):
     visit = list(set(gems))
-    visit_bool = [False]*(len(visit))
+    m = len(gems)+1
     for i in range(len(gems)-len(visit)+1):
-        
-
-
-
-    return 
+        visit_bool = [1]*(len(visit))
+        visit_bool[visit.index(gems[i])] = 0
+        start = i
+        while sum(visit_bool) != 0:
+            i += 1
+            if i-start > m:
+                break
+            if i<len(gems): 
+                if visit_bool[visit.index(gems[i])]:
+                    visit_bool[visit.index(gems[i])] = 0
+            else:
+                break
+        else:
+            end = i
+            if (end-start)<m:
+                m = end-start
+                result = [start+1, end+1]
+    return result
 
 
 G = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
